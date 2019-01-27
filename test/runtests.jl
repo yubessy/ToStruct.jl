2d45223db1cceb779e60de072a10b77d5480529e
yubessy <yubessy0@gmail.com>
2019-01-27 23:22:06 +0900

    change argument order

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,26 +13,26 @@ struct S1
 end
 
 @testset "struct" begin
-    @test tostruct(Dict(
+    @test tostruct(S1, Dict(
         "i" => 1,
         "s2" => Dict(
             "str" => "foo",
             "int" => 1
         )
-    ), S1) == S1(
+    )) == S1(
         1,
         S2(
             "foo",
             1
         )
     )
-    @test tostruct(Dict{Any,Any}(
+    @test tostruct(S1, Dict{Any,Any}(
         "i" => 1,
         "s2" => Dict{Any,Any}(
             "str" => "foo",
             "int" => 1
         )
-    ), S1) == S1(
+    )) == S1(
         1,
         S2(
             "foo",
@@ -42,35 +42,35 @@ end
 end
 
 @testset "dict" begin
-    @test tostruct(Dict("a" => 1, "b" => 2, "c" => 3), Dict) == Dict("a" => 1, "b" => 2, "c" => 3)
-    @test tostruct(Dict(
+    @test tostruct(Dict, Dict("a" => 1, "b" => 2, "c" => 3)) == Dict("a" => 1, "b" => 2, "c" => 3)
+    @test tostruct(Dict{String,S2}, Dict(
         "a" => Dict("str" => "foo", "int" => 1),
         "b" => Dict("str" => "bar", "int" => 2),
-    ), Dict{String,S2}) == Dict(
+    )) == Dict(
         "a" => S2("foo", 1),
         "b" => S2("bar", 2),
     )
 end
 
 @testset "array" begin
-    @test tostruct([1, 2, 3], Vector) == [1, 2, 3]
-    @test tostruct([
+    @test tostruct(Vector, [1, 2, 3]) == [1, 2, 3]
+    @test tostruct(Vector{S2}, [
         Dict("str" => "foo", "int" => 1),
         Dict("str" => "bar", "int" => 2),
-    ], Vector{S2}) == [
+    ]) == [
         S2("foo", 1),
         S2("bar", 2),
     ]
 end
 
 @testset "union" begin
-    @test tostruct(1, Union{Nothing,Int}) == 1
-    @test tostruct(nothing, Union{Nothing,Int}) == nothing
+    @test tostruct(Union{Nothing,Int}, 1) == 1
+    @test tostruct(Union{Nothing,Int}, nothing) == nothing
 end
 
 @testset "primitive" begin
-    @test tostruct(1, Int) == 1
-    @test tostruct(1., Float64) == 1.
-    @test tostruct("foo", String) == "foo"
-    @test tostruct(1, Float64) == 1.
+    @test tostruct(Int, 1) == 1
+    @test tostruct(Float64, 1.) == 1.
+    @test tostruct(String, "foo") == "foo"
+    @test tostruct(Float64, 1) == 1.
 end
